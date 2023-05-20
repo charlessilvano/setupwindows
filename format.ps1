6ae9df16917494a2e0c870e4094b1c9f0176c9f8
Charles Silvano <13371419+charlessilvano@users.noreply.github.com>
2023-05-20 06:39:26 -0300

    Atualizar o format.ps1

--- format.ps1
+++ format.ps1
@@ -1,28 +1,30 @@
 #Desabilitar as restrições do Powershell
 Set-ExecutionPolicy Unrestricted
 #Fazer download do pacote WinGet
-Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "C:\Users\Usuário\Downloads\Winget.msixbundle"
+Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "$env:USERPROFILE\Downloads\Winget.msixbundle"
 #Executar instalação do pacote WinGet
-Add-AppxPackage -Path "C:\Users\Usuário\Downloads\Winget.msixbundle"
+Add-AppxPackage -Path "$env:USERPROFILE\Downloads\Winget.msixbundle"
 #Criando um direto de arquivos de mídia
 New-Item -ItemType Directory -Path "C:\Windows\Web\Charles Silvano"
 #Configurando o usuário para o grupo Administradores
 Add-LocalGroupMember -Group "Administradores" -Member "Usuário"
 #Fazer downloads dos arquivos user e wallpaper
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/charlessilvano/setupwindows/main/files/user.png" -OutFile "C:\Users\Usuário\Downloads\user.png"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/charlessilvano/setupwindows/main/files/wallpaper.png" -OutFile "C:\Users\Usuário\Downloads\wallpaper.png"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/charlessilvano/setupwindows/main/files/user.png" -OutFile "$env:USERPROFILE\Downloads\user.png"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/charlessilvano/setupwindows/main/files/wallpaper.png" -OutFile "$env:USERPROFILE\Downloads\wallpaper.png"
 #Copiando os arquivos para a pasta de mídia
-Copy-Item -Path "C:\Users\Usuário\Downloads\user.png" -Destination "C:\Windows\Web\Charles Silvano"
-Copy-Item -Path "C:\Users\Usuário\Downloads\wallpaper.png" -Destination "C:\Windows\Web\Charles Silvano"
-
+Copy-Item -Path "$env:USERPROFILE\Downloads\user.png" -Destination "C:\Windows\Web\Charles Silvano"
+Copy-Item -Path "$env:USERPROFILE\Downloads\wallpaper.png" -Destination "C:\Windows\Web\Charles Silvano"
+#Configurando a foto de usuário e papel de parede
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization' -Name LockScreenImage -Value 'C:\Windows\Web\Charles Silvano\wallpaper.png'
 Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name LockScreenImage -Value 'C:\Windows\Web\Charles Silvano\wallpaper.png'
 Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name Wallpaper -Value 'C:\Windows\Web\Charles Silvano\wallpaper.png'
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization' -Name Wallpaper -Value 'C:\Windows\Web\Charles Silvano\wallpaper.png'
+#Instalando programas padrões
 winget install 7zip.7zip --silent
 winget install Google.Chrome --silent
 winget install XPDM1ZW6815MQM --silent
 winget install XPDP273C0XHQH2 --silent
+#Removendo programas sem utilidade
 winget uninstall microsoft.onedrive --silent
 winget uninstall Microsoft.BingNews_8wekyb3d8bbwe --silent
 winget uninstall Microsoft.GetHelp_8wekyb3d8bbwe --silent
@@ -32,13 +34,13 @@ winget uninstall Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe --silent
 winget uninstall Microsoft.PowerAutomateDesktop_8wekyb3d8bbwe --silent
 winget uninstall Microsoft.Todos_8wekyb3d8bbwe --silent
 winget uninstall Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe --silent
-
-
- $NewName = Read-Host -Promp "Digite o novo nome de usuário"
+#Configurando nome de usuário
+$NewName = Read-Host -Promp "Digite o novo nome de usuário"
 Rename-LocalUser -Name "Usuário" -NewName $NewName
+#Configurando nome do computador
 $Computador = Read-Host "Digite o novo nome do Computador"
 Rename-Computer -NewName $Computador
-
+#Realizando atualizações pelo Windows Update
 Install-Module PSWindowsUpdate
 Get-WindowsUpdate
 Install-WindowsUpdate
