42d78aa8cdb88d236c65fbb1678a976a94677b3d
Charles Silvano <13371419+charlessilvano@users.noreply.github.com>
2023-07-24 12:11:59 -0300

    Atualizar o maintenance.ps1

--- maintenance.ps1
+++ maintenance.ps1
@@ -1,1 +1,5 @@
-
+# Fazer a limpeza dos arquivos temporários
+Remove-Item -Path C:\Windows\Temp -Recurse
+Remove-Item -Path C:\Windows\System32\Temp -Recurse
+Remove-Item -Path $env:USERPROFILE\AppData\Local\Temp -Recurse
+Remove-Item -Path $env:USERPROFILE\AppData\Local\Microsoft\Windows\Temporary Internet Files -Recurse
