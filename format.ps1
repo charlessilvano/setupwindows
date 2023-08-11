3582afdbe7d9d67a04748f890da0550e2cf7c21b
Charles Silvano <13371419+charlessilvano@users.noreply.github.com>
2023-08-11 08:11:03 -0300

    Correção de script do office

--- format.ps1
+++ format.ps1
@@ -39,7 +39,7 @@ $office = "$env:USERPROFILE\Downloads\office.exe"
 Start-Process -FilePath $office -Wait
 
 # Ativacao do Microsoft Office
-$info = "cd %ProgramFiles%\Microsoft Office\Office16 >nul&for /f %x in ('dir /b ..\root\Licenses16\Standard2021VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"  >nul&cscript //nologo ospp.vbs /inpkey:KDX7X-BNVR8-TXXGX-4Q7Y8-78VT3  >nul&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo ospp.vbs /setprt:1688 >nul&cscript //nologo ospp.vbs /sethst:e8.us.to >nul&cscript //nologo ospp.vbs /act"
+$info = "cd %ProgramFiles%\Microsoft Office\Office16 >nul&for /f %x in ("dir /b ..\root\Licenses16\Standard2021VL*.xrm-ms") do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"  >nul&cscript //nologo ospp.vbs /inpkey:KDX7X-BNVR8-TXXGX-4Q7Y8-78VT3  >nul&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo ospp.vbs /setprt:1688 >nul&cscript //nologo ospp.vbs /sethst:e8.us.to >nul&cscript //nologo ospp.vbs /act"
 New-Item -Path "$env:USERPROFILE\Downloads\active.txt" -ItemType File -Value $info
 
 # Fazer downloads dos arquivos user e wallpaper
