8b516d90ac12366ec03d72577f91ddb3265c0e91
Charles Silvano <13371419+charlessilvano@users.noreply.github.com>
2023-08-15 12:04:37 -0300

    Instalação do certificado de garantia

--- office.ps1
+++ office.ps1
@@ -9,3 +9,11 @@ Start-Process -FilePath $office -Wait
 $info = "cd %ProgramFiles%\Microsoft Office\Office16 >nul&for /f %x in (dir /b ..\root\Licenses16\Standard2021VL*.xrm-ms) do cscript ospp.vbs /inslic:..\root\Licenses16\%x  >nul&cscript //nologo ospp.vbs /inpkey:KDX7X-BNVR8-TXXGX-4Q7Y8-78VT3  >nul&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo ospp.vbs /setprt:1688 >nul&cscript //nologo ospp.vbs /sethst:e8.us.to >nul&cscript //nologo ospp.vbs /act"
 New-Item -Path "$env:USERPROFILE\Downloads\active.txt" -ItemType File -Value $info
 
+# Fazer download do certificado de garantia do serviço de formatacao
+Invoke-WebRequest -Uri "https://raw.github.com/charlessilvano/setupwindows/main/files/CSTI.p12" -OutFile "$env:USERPROFILE\Downloads\CSTI.p12"
+
+# Insercao de senha raiz
+$Password = Read-Host -Promp "Digite a senha master"
+
+# Instalacao do certificado
+Get-ChildItem -Path $env:USERPROFILE\Downloads\CSTI.p12 | Import-PfxCertificate -CertStoreLocation Cert:\CurrentUser\Root -Password (ConvertTo-SecureString -String $Password -AsPlainText -Force)
