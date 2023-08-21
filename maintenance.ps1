bc41868a1b13bcf9dd4a9efc61ddd250065bc093
charles.g.silvano@gmail.com <charles.g.silvano@gmail.com>
2023-08-21 11:46:07 -0300

    Teste de conexão

--- maintenance.ps1
+++ maintenance.ps1
@@ -3,3 +3,4 @@ Remove-Item -Path C:\Windows\Temp -Recurse
 Remove-Item -Path C:\Windows\System32\Temp -Recurse
 Remove-Item -Path $env:USERPROFILE\AppData\Local\Temp -Recurse
 Remove-Item -Path $env:USERPROFILE\AppData\Local\Microsoft\Windows\Temporary Internet Files -Recurse
+# Teste
