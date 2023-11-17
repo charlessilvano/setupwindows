8cb924f6a56ab902b2d3fa0cb6ff5a8c2e90dcbe
Charles Silvano <13371419+charlessilvano@users.noreply.github.com>
2023-11-17 10:56:31 -0300

    Configuração para limpeza de arquivos

--- maintenance.ps1
+++ maintenance.ps1
@@ -1,6 +1,6 @@
 # Fazer a limpeza dos arquivos temporários
-Remove-Item -Path C:\Windows\Temp -Recurse
-Remove-Item -Path C:\Windows\System32\Temp -Recurse
-Remove-Item -Path $env:USERPROFILE\AppData\Local\Temp -Recurse
-Remove-Item -Path $env:USERPROFILE\AppData\Local\Microsoft\Windows\Temporary Internet Files -Recurse
-# Teste
+Remove-Item -Path "C:\Temp" -Recurse
+Remove-Item -Path "C:\Windows\Temp\*" -Recurse
+Remove-Item -Path "C:\Windows\System32\LogFiles\*" -Recurse
+Remove-Item -Path "$env:USERPROFILE\AppData\Local\Temp\*" -Recurse
+
