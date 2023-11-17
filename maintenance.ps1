7921118ae0b91d1ad3174a0ccd3f53c8eed4bb7b
Charles Silvano <13371419+charlessilvano@users.noreply.github.com>
2023-11-17 12:09:13 -0300

    Correção de localização

--- maintenance.ps1
+++ maintenance.ps1
@@ -5,8 +5,9 @@ Set-ExecutionPolicy Unrestricted
 Set-PSRepository -Name PSGallery -InstalationPolicy Trusted
 
 # Download e Instalacao do DriverEasy
-Invoke-WebRequest -Uri "https://www.drivereasy.com/DriverEasy_Setup.exe" -OutFile 'C:\Temp\setup.exe'
-$drivereasy = 'C:\Temp\setup.exe'
+cd C:\Temp
+Invoke-WebRequest -Uri "https://www.drivereasy.com/DriverEasy_Setup.exe" -OutFile setup.exe
+$drivereasy = setup.exe
 Start-Process -FilePath $drivereasy -Wait
 
 # Instalacao do modulo de atualizacao do Windows Update pelo Powershell
