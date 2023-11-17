030bdca23da9630065e19f56cbd2c0a5d0315537
Charles Silvano <13371419+charlessilvano@users.noreply.github.com>
2023-11-17 11:51:01 -0300

    Atualização de Procedimentos

--- maintenance.ps1
+++ maintenance.ps1
@@ -1,6 +1,16 @@
+# Download e Instalção do DriverEasy
+Invoke-WebRequest -Uri "https://www.drivereasy.com/DriverEasy_Setup.exe" -OutFile C:\Temp\setup.exe
+$drivereasy = C:\Temp\setup.exe
+Start-Process -FilePath $drivereasy -Wait
+
+# Instalção do módulo de atualização do Windows Update pelo Powershell
+Install-Module PSWindowsUpdate
+Get-WindowsUpdate
+Install-WindowsUpdate
+
 # Fazer a limpeza dos arquivos temporários
 Remove-Item -Path "C:\Temp" -Recurse
 Remove-Item -Path "C:\Windows\Temp\*" -Recurse
 Remove-Item -Path "C:\Windows\System32\LogFiles\*" -Recurse
+Remove-Item -Path "C:\Windows\SoftwareDistribution\Download\*" -Recurse
 Remove-Item -Path "$env:USERPROFILE\AppData\Local\Temp\*" -Recurse
-
