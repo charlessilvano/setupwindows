a1244e22d568961c4578e65c2c30ac4a71dbf962
Charles Silvano <13371419+charlessilvano@users.noreply.github.com>
2023-11-19 15:22:51 -0300

    Exclusão de script de manutenção temporário

--- maintenance 1.ps1
+++
@@ -1,5 +0,0 @@
-# Fazer a limpeza dos arquivos temporários
-Remove-Item -Path C:\Windows\Temp -Recurse
-Remove-Item -Path C:\Windows\System32\Temp -Recurse
-Remove-Item -Path $env:USERPROFILE\AppData\Local\Temp -Recurse
-Remove-Item -Path $env:USERPROFILE\AppData\Local\Microsoft\Windows\Temporary Internet Files -Recurse
